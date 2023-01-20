6343c3683f7a242e4edae4b8f0a640825fab0b5f
Justin Stitt <cpt.crixus@gmail.com>
2023-01-19 22:39:23 -0800

    add extra courses, prelim

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,14 @@
 
   import BurgerMenu from "./components/svelte-burger-menu/src/BurgerMenu.svelte";
   // TODO:justinstitt fetch these from backend (requires getting CWID first, then displaying courses)
-  const courses = ["CPSC-120", "CPSC-121", "CPSC-131", "CPSC-240"];
+  const courses = [
+    "CPSC-120",
+    "CPSC-121",
+    "CPSC-131",
+    "CPSC-240",
+    "BIOL-151",
+    "MATH-135",
+  ];
 
   let cwid_inp = "";
   let course_inp = "";
