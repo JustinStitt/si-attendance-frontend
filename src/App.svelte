0715d6fdc3e612b50c8313f65720dfaa7cc9bb00
Justin Stitt <jstitt007@gmail.com>
2024-01-26 21:45:01 -0800

    remove MATH-135 add MATH-115

--- src/App.svelte
+++ src/App.svelte
@@ -4,14 +4,7 @@
 
   import BurgerMenu from "./components/svelte-burger-menu/src/BurgerMenu.svelte";
   // TODO:justinstitt fetch these from backend (requires getting CWID first, then displaying courses)
-  const courses = [
-    "CPSC-120",
-    "CPSC-121",
-    "CPSC-131",
-    "CPSC-240",
-    "BIOL-151",
-    "MATH-135",
-  ];
+  const courses = ["CPSC-120", "CPSC-121", "CPSC-131", "CPSC-240", "MATH-115"];
 
   let cwid_inp = "";
   let course_inp = "";
